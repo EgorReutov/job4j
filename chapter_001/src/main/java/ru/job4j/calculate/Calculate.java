f90eaf9a46517cdcd947261d8e96b4de5dc89292
EgorReutov <mailto:reutov9@yandex.ru>
2018-07-09 21:34:25 +0300

    1.3. JavaDoc. Code convention[#60639]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,7 +1,21 @@
+/**
+* Package for calculate task.
+*
+* @author Egor Reutov (mailto:reutov9@yandex.ru)
+* @version $Id$
+* @since 0.1
+*/
+
 package ru.job4j.calculate;
 
+/**
+* Class Класс для выведения на экран "Hello World"
+* @since 08.07.2018
+*/
 public class Calculate {
-
+/**
+* @param main выводит на экран "Hello World"
+*/
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 	}
