34070ec7ad69f0178f853cee03c45db0d1da8391
EgorReutov <mailto:reutov9@yandex.ru>
2018-07-10 12:14:42 +0300

    1.3.1 Maven Plugins[#60640]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -9,14 +9,24 @@
 package ru.job4j.calculate;
 
 /**
-* Class Класс для выведения на экран "Hello World"
-* @since 08.07.2018
+* Main.	
+* @param args - args.
 */
 public class Calculate {
+
 /**
 * @param main выводит на экран "Hello World"
 */
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 	}
+	
+/**
+* Method echo.
+* @param name Egor Reutov.
+* @return Echo plus your name.
+*/
+public String echo(String name) {
+    return "Echo, echo, echo : " + name;
+}	
 }
