6afe4962932de9b5804fb93b6957452751be01d7
EgorReutov <mailto:reutov9@yandex.ru>
2018-07-12 13:42:42 +0300

    add test

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,32 +1,32 @@
 /**
-* Package for calculate task.
-*
-* @author Egor Reutov (mailto:reutov9@yandex.ru)
-* @version $Id$
-* @since 0.1
-*/
+ * Package for calculate task.
+ *
+ * @author Egor Reutov (mailto:reutov9@yandex.ru)
+ * @version $Id$
+ * @since 0.1
+ */
 
 package ru.job4j.calculate;
 
 /**
-* Main.	
-* @param args - args.
-*/
+ * Main.
+ * @param args - args.
+ */
 public class Calculate {
 
-/**
-* @param main выводит на экран "Hello World"
-*/
+	/**
+	 * @param main выводит на экран "Hello World"
+	 */
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 	}
-	
-/**
-* Method echo.
-* @param name Egor Reutov.
-* @return Echo plus your name.
-*/
-public String echo(String name) {
-    return "Echo, echo, echo : " + name;
-}	
+
+	/**
+	 * Method echo.
+	 * @param name Egor Reutov.
+	 * @return Echo plus your name.
+	 */
+	public String echo(String name) {
+		return "Echo, echo, echo : " + name;
+	}
 }
