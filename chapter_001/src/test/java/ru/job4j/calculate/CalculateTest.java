516bb32ef1d382730cc22d0e8b18e913a899e726
EgorReutov <mailto:reutov9@yandex.ru>
2018-07-12 23:44:59 +0300

    test problems

--- chapter_001/src/test/java/ru/job4j/calculate/CalculateTest.java
+++ chapter_001/src/test/java/ru/job4j/calculate/CalculateTest.java
@@ -3,24 +3,23 @@ package ru.job4j.calculate;
 import org.junit.Test;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
-
 /**
-* Test.
-*
-* @author Egor Reutov (mailto:reutov9@yandex.ru)
-* @version $Id$
-* @since 0.1
-*/
+ * Test.
+ *
+ * @author Egor Reutov (mailto:reutov9@yandex.ru)
+ * @version $Id$
+ * @since 0.1
+ */
 public class CalculateTest {
-/**
-* Test echo.
-*/ @Test
-public void whenTakeNameThenTreeEchoPlusName() {
-    String input = "Petr Arsentev";
-    String expect = "Echo, echo, echo : Petr Arsentev"; 
-    Calculate calc = new Calculate();
-    String result = calc.echo(input);
-    assertThat(result, is(expect));
-}
- 
+    /**
+     * Test echo.
+     */ @Test
+    public void whenTakeNameThenTreeEchoPlusName() {
+        String input = "Petr Arsentev";
+        String expect = "Echo, echo, echo : Petr Arsentev";
+        Calculate calc = new Calculate();
+        String result = calc.echo(input);
+        assertThat(result, is(expect));
+    }
+
 }
--- chapter_001/src/test/java/ru/job4j/calculate/package-info.java
+++ chapter_001/src/test/java/ru/job4j/calculate/package-info.java
@@ -1,8 +1,8 @@
 /**
-* Package for calculate task.
-*
-* @author Egor Reutov (mailto:reutov9@yandex.ru)
-* @version $Id$
-* @since 0.1
-*/
+ * Package for calculate task.
+ *
+ * @author Egor Reutov (mailto:reutov9@yandex.ru)
+ * @version $Id$
+ * @since 0.1
+ */
 package test.java.ru.job4j.calculate;
