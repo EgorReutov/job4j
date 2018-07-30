8ec2e9f61d23f41755ddb5717125bee7d6fa0015
EgorReutov <mailto:reutov9@yandex.ru>
2018-07-30 10:45:48 +0300

    Крестики-нолики на JavaFX [#63871]

--- chapter_001/src/main/java/ru/job4j/tictactie/Logic3T.java
+++ chapter_001/src/main/java/ru/job4j/tictactie/Logic3T.java
@@ -1,1 +1,67 @@
 package ru.job4j.tictactie;
+
+public class Logic3T {
+    private final Figure3T[][] table;
+
+    public Logic3T(Figure3T[][] table) {
+        this.table = table;
+    }
+
+    public boolean isWinnerX() {
+        boolean result = false;
+        for (int index = 0; index < table.length - 1; index++) {
+            if (table[index][index] == table[index + 1][index + 1]) {
+                result = true;
+                break;
+            }
+            if (table[table.length - 1 - index][index] == table[table.length - 2 - index][index + 1]) {
+                result = true;
+                break;
+            }
+            for (int i = 0; i < table.length; i++) {
+                for (int j = 0; j < table.length; j++) {
+                    if (table[i][j] == table[i][index + 1]){
+                        result = true;
+                        break;
+                    }
+                }
+            }
+        }
+        return result;
+    }
+
+    public boolean isWinnerO() {
+        boolean result = false;
+        for (int index = 0; index < table.length - 1; index++) {
+            if (table[index][index] == table[index + 1][index + 1]) {
+                result = true;
+                break;
+            }
+            if (table[table.length - 1 - index][index] == table[table.length - 2 - index][index + 1]) {
+                result = true;
+                break;
+            }
+            for (int i = 0; i < table.length; i++) {
+                for (int j = 0; j < table.length; j++) {
+                    if (table[i][j] == table[i][index + 1]){
+                        result = true;
+                        break;
+                    }
+                }
+            }
+        }
+        return result;
+    }
+
+    public boolean hasGap() {
+        boolean result = false;
+        for (int i = 0; i < table.length; i++) {
+            for (int j = 0; j < table[i].length; j++) {
+                if (table[i][j].hasMarkX() == table[i][j].hasMarkO()) {
+                    return true;
+                }
+            }
+        }
+        return result;
+    }
+}
