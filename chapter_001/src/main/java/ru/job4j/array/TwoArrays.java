fc9c5cc9908f8eb002c911b1c406f55c66156266
EgorReutov <mailto:reutov9@yandex.ru>
2018-08-28 16:44:33 +0300

    Задания от Петра #2[#63629] 2.0

--- chapter_001/src/main/java/ru/job4j/array/TwoArrays.java
+++ chapter_001/src/main/java/ru/job4j/array/TwoArrays.java
@@ -3,15 +3,28 @@ package ru.job4j.array;
 public class TwoArrays {
     public int[] first(int[] arrayOne, int[] arrayTwo) {
         int[] result = new int[arrayOne.length + arrayTwo.length];
-        for (int index: first(arrayOne, arrayTwo)) {
-            if (arrayOne[index] < arrayTwo[index]) {
-                result[index] = arrayOne[index];
-                result[index + 1] = arrayTwo[index];
+        int i = 0, j = 0;
+        for (int k = 0; k < result.length; k++) {
+
+            if (i > arrayOne.length - 1) {
+                int a = arrayTwo[j];
+                result[k] = a;
+                j++;
+            } else if (j > arrayTwo.length - 1) {
+                int a = arrayOne[i];
+                result[k] = a;
+                i++;
+            } else if (arrayOne[i] < arrayTwo[j]) {
+                int a = arrayOne[i];
+                result[k] = a;
+                i++;
             } else {
-                result[index] = arrayTwo[index];
-                result[index + 1] = arrayOne[index];
+                int b = arrayTwo[j];
+                result[k] = b;
+                j++;
             }
         }
         return result;
     }
 }
+
