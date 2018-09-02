7d153608c19f4a4c717aadbbdb351d49df0af246
EgorReutov <mailto:reutov9@yandex.ru>
2018-09-02 10:11:05 +0300

    Задания от Петра #1 4.0

--- chapter_001/src/main/java/ru/job4j/array/TwoArrays.java
+++ chapter_001/src/main/java/ru/job4j/array/TwoArrays.java
@@ -5,12 +5,9 @@ public class TwoArrays {
         int[] result = new int[arrayOne.length + arrayTwo.length];
         int i = 0, j = 0;
         for (int k = 0; k < result.length; k++) {
-
             if (i > arrayOne.length - 1) {
                 result[k] = arrayTwo[j++];
-            } else if (j > arrayTwo.length - 1) {
-                result[k] = arrayOne[i++];
-            } else if (arrayOne[i] < arrayTwo[j]) {
+            } else if (j > arrayTwo.length - 1 || arrayOne[i] < arrayTwo[j]) {
                 result[k] = arrayOne[i++];
             } else {
                 result[k] = arrayTwo[j++];
