762abe3bbe5d246fbe29854c5d8a00477261d498
EgorReutov <reutov9@yandex.ru>
2018-11-05 20:22:41 +0300

    Организовать сортировку User

--- chapter_002/src/main/java/ru/job4j/sort/User.java
+++ chapter_002/src/main/java/ru/job4j/sort/User.java
@@ -17,19 +17,26 @@ public class User implements Comparable<User> {
     }
 
     @Override
-    public int hashCode() {
-        return this.age.hashCode();
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        User user = (User) o;
+        return Objects.equals(age, user.age) &&
+                Objects.equals(name, user.name);
     }
 
     @Override
-    public boolean equals(Object obj) {
-        return super.equals(obj);
+    public int hashCode() {
+        return Objects.hash(age, name);
     }
 
     @Override
     public int compareTo(User o) {
-        return this.age.compareTo(o.getAge());
+        return this.age.compareTo(o.age);
 
     }
-
 }
--- chapter_002/src/test/java/ru/job4j/sort/SortUserTest.java
+++ chapter_002/src/test/java/ru/job4j/sort/SortUserTest.java
@@ -2,56 +2,25 @@ package ru.job4j.sort;
 
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.TreeSet;
+import java.util.*;
 
 import static java.util.Collections.sort;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
 public class SortUserTest {
- /*   @Test
-    public void sortThreeUser() {
+    @Test
+    public void sortThreeUser1() {
         SortUser sortUsers = new SortUser();
         List<User> usersNoSort = new ArrayList<>();
-        usersNoSort.addAll(
-                Arrays.asList(
-                        new User("alina", 21),
-                        new User("petr", 31),
-                        new User("egor", 10)
-                )
-        );
-        List<User> usersSort = new ArrayList<>();
+        User alina = new User("alina", 21);
+        User petr = new User("petr", 31);
+        User egor = new User("egor", 10);
+        usersNoSort.addAll(Arrays.asList(alina, petr, egor));
+        Set<User> usersSort = new TreeSet<>();
         usersSort.addAll(sortUsers.sort(usersNoSort));
-        List<User> expect = new ArrayList<>();
-        expect.addAll(
-                Arrays.asList(
-                        new User("egor", 10),
-                        new User("alina", 21),
-                        new User("petr", 31)
-                        )
-        );
+        Set<User> expect = new TreeSet<>();
+        expect.addAll(Arrays.asList(egor, alina, petr));
         assertThat(usersSort,is(expect));
-    }
-*/
-    @Test
-    public void sortThreeUser() {
-        TreeSet<User> result = new TreeSet<>();
-        TreeSet<User> expect = new TreeSet<>();
-        List<User> list = new ArrayList<>();
-        SortUser sortUser = new SortUser();
-        User user1 = new User("egor", 21);
-        User user2 = new User("petr", 31);
-        User user3 = new User("alina", 10);
-        list.add(user1);
-        list.add(user2);
-        list.add(user3);
-        expect.add(user3);
-        expect.add(user1);
-        expect.add(user2);
-        result.addAll(sortUser.sort(list));
-        assertThat(result, is(expect));
     }
 }
