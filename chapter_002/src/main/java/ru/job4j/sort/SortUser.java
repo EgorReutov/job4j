57ba8e8100b2cf225f9d19a980cd9c9a25747e12
EgorReutov <reutov9@yandex.ru>
2018-11-20 21:06:36 +0300

    5.1.1. Итератор для двухмерного массива int[][]

--- chapter_002/src/main/java/ru/job4j/sort/SortUser.java
+++ chapter_002/src/main/java/ru/job4j/sort/SortUser.java
@@ -4,8 +4,7 @@ import java.util.*;
 
 public class SortUser {
     public Set<User> sort(List<User> list) {
-        Set<User> result = new TreeSet<>(list);
-        return result;
+        return new TreeSet<>(list);
     }
 
     public List<User> sortNameLength(List<User> list) {
