e0c3f202e8d017830b7e27fcec574b534f29032e
EgorReutov <reutov9@yandex.ru>
2018-12-01 18:07:21 +0300

    5.3.1. Создать динамический список на базе массива.

---
+++ chapter_003/src/main/java/ru/job4j/list/DynamicList.java
@@ -0,0 +1,54 @@
+package ru.job4j.list;
+
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class DynamicList<E> implements Iterable<E> {
+    private int index = 0;
+    private int modCount = 0;
+    private E[] array = (E[]) new Object[5];
+
+    public void add(E object) {
+        if(index == array.length - 1) {
+            reSize((array.length * 3) / 2 + 1);
+        }
+        modCount++;
+        array[index++] = object;
+    }
+
+    public E get(int index) {
+        return array[index];
+    }
+
+    private void reSize(int newSize) {
+        E[] newArray = (E[]) new Object[newSize];
+        System.arraycopy(array, 0, newArray, 0, index);
+        array = newArray;
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return new Iterator<E>() {
+            int expectedModCount = modCount;
+            int counter = 0;
+
+            @Override
+            public boolean hasNext() {
+                return counter < array.length;
+            }
+
+            @Override
+            public E next() {
+                if (modCount != expectedModCount) {
+                    throw new ConcurrentModificationException();
+                }
+
+                if (counter >= array.length) {
+                    throw new NoSuchElementException();
+                }
+                return array[counter++];
+            }
+        };
+    }
+}
---
+++ chapter_003/src/test/java/ru/job4j/list/DynamicListTest.java
@@ -0,0 +1,42 @@
+package ru.job4j.list;
+
+import org.junit.Test;
+
+import java.util.Iterator;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+
+public class DynamicListTest {
+
+    private DynamicList<Object> list = new DynamicList<>();
+
+    @Test
+    public void testAdd() {
+        list.add(5);
+        list.add(8);
+        assertThat(list.get(1), is(8));
+    }
+
+    @Test
+    public void testIterator() {
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        Iterator<Object> iterator = list.iterator();
+        assertThat(iterator.hasNext(), is( true));
+        assertThat(iterator.next(), is( 1));
+        assertThat(iterator.hasNext(), is( true));
+        assertThat(iterator.next(), is( 2));
+        assertThat(iterator.hasNext(), is( true));
+        assertThat(iterator.next(), is( 3));
+        assertThat(iterator.hasNext(), is( true));
+        assertThat(iterator.next(), is( 4));
+        assertThat(iterator.hasNext(), is( true));
+        assertThat(iterator.next(), is( 5));
+        assertThat(iterator.hasNext(), is( true));
+
+    }
+}
