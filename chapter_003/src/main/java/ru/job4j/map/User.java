2756d705ac86c7faf0d687052ae1dc4940f75845
EgorReutov <reutov9@yandex.ru>
2018-12-03 16:02:37 +0300

    3. Переопределить только hashCode

--- chapter_003/src/main/java/ru/job4j/map/User.java
+++ chapter_003/src/main/java/ru/job4j/map/User.java
@@ -1,14 +1,18 @@
 package ru.job4j.map;
 
-import java.util.Calendar;
-import java.util.GregorianCalendar;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class User {
     private String name;
     private int children;
     private Calendar birthday;
+
+    @Override
+    public int hashCode() {
+        int result = name != null ? name.hashCode() : 0;
+        result = 31 * result + children;
+        return result;
+    }
 
     public User(String name, int children, Calendar birthday) {
         this.name = name;
@@ -17,9 +21,11 @@ public class User {
     }
 
     public static void main(String[] args) {
-        User first = new User("Egor", 0, new GregorianCalendar(1997, 01, 9));
-        User second = new User("Egor", 0, new GregorianCalendar(1997, 01, 9));
+        Calendar calendar = new GregorianCalendar(1997, 01, 9);
+        User first = new User("Egor", 0, calendar);
+        User second = new User("Egor", 0, calendar);
         Map<User, Object> map = new HashMap<>();
+
         map.put(first, "first");
         map.put(second, "second");
         System.out.println(map);
