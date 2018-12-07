99352c33479af80ce300785992e16ccc0ba16d29
EgorReutov <reutov9@yandex.ru>
2018-12-07 22:26:25 +0300

    9.1. Рассказать и продемонстрировать как переопределяют метод hashCode

--- chapter_003/src/main/java/ru/job4j/map/Person.java
+++ chapter_003/src/main/java/ru/job4j/map/Person.java
@@ -5,11 +5,19 @@ public class Person {
     private String firstName;
     private String lastName;
 
-    public int getId() { return id; }
-    public void setId(int id) { this.id = id;}
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
 
-    public String getFirstName() { return firstName; }
-    public String getLastName() { return lastName; }
+    public String getFirstName() {
+        return firstName;
+    }
+    public String getLastName() {
+        return lastName;
+    }
 
     @Override
     public boolean equals(Object obj) {
@@ -26,5 +34,14 @@ public class Person {
                 || (firstName != null && firstName.equals(guest.getFirstName())))
                 && (lastName == guest.lastName || (lastName != null && lastName .equals(guest.getLastName())
         ));
+    }
+
+    @Override
+    public int hashCode() {
+        int result = 17;
+        result = 31 * result + ((firstName == null) ? 0 : firstName.hashCode());
+        result = 31 * result + id;
+        result = 31 * result + ((lastName == null) ? 0 : lastName.hashCode());
+        return result;
     }
 }
